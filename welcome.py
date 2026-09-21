0dc8e01fa42b4658342684e84f6dae34b97f48dd
zamzamjirow <simangclabdi@gmail.com>
2026-09-21 16:35:36 +0300

    Delete welcome.py

--- welcome.py
+++
@@ -1,8 +0,0 @@
-name = "Amina"
-print("Hello, " + name + "! Welcome to PLP.")
-
-name = "Brian"
-print("Hello, " + name + "! Welcome to PLP.")
-
-name = "Fatuma"
-print("Hello, " + name + "! Welcome to PLP.")
